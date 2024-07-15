615203bca64f7a595a194c0d35a890f6f59a4ac5
watanabe@marida <watanabe.everystudio@gmail.com>
2024-07-16 00:10:32 +0900

    Headbob修正 footstepを追加する

--- Assets/Scripts/FirstPersonHeadbob.cs
+++ Assets/Scripts/FirstPersonHeadbob.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class FirstPersonHeadbob : MonoBehaviour
 {
-    public float swingSpeed = 3.5f;
+    public float swingSpeed = 200f;
     public float swingAmount = 0.025f;
-    private float timer = 0.0f;
+    private float degree = 0.0f;
+
+
+    [HideInInspector]
+    public UnityEvent OnFootStep = new UnityEvent();
 
     public void Headbob(FirstPersonController firstPersonController, Transform cameraTransform, Vector3 velocity, float deltaTime)
     {
@@ -20,10 +24,18 @@ public class FirstPersonHeadbob : MonoBehaviour
         if (0.1f < velocityMagnitude)
         {
             float stateBonus = firstPersonController.IsSprinting ? 1.25f : firstPersonController.IsCrouching ? 0.5f : 1.0f;
-            timer += deltaTime * velocityMagnitude * swingSpeed * stateBonus;
+            float currentDegree = Mathf.Repeat(degree + deltaTime * velocityMagnitude * swingSpeed * stateBonus, 360.0f);
+            float currentRadian = currentDegree * Mathf.Deg2Rad;
+
+            if (degree < 270f && 270f <= currentDegree)
+            {
+                OnFootStep.Invoke();
+            }
+            degree = currentDegree;
+
             cameraTransform.localPosition = new Vector3(
                 cameraTransform.localPosition.x,
-                Mathf.Sin(timer) * swingAmount * stateBonus,
+                Mathf.Sin(currentRadian) * swingAmount * stateBonus,
                 cameraTransform.localPosition.z);
         }
 
